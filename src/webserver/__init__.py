55b32936936b332a1730cdf485ddc05ca0a0951f
Jeff <j.king.hou+selkirk@gmail.com>
2017-09-07 03:50:10 +0000

    Added form for submitting summoner names (with default values)

--- src/webserver/__init__.py
+++ src/webserver/__init__.py
@@ -1,9 +1,21 @@
 """
 Basic module which creates the app
 """
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+
 myApp = Flask(__name__)
 
 @myApp.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("entry.html")
+
+@myApp.route('/process_summoner_names', methods=["GET"])
+def process_summoner_names():
+    summoner_names = [
+        request.args.get('summoner1'),
+        request.args.get('summoner2'),
+        request.args.get('summoner3'),
+        request.args.get('summoner4'),
+        request.args.get('summoner5')
+    ]
+    return str(summoner_names)
